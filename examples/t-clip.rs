931eebbbf5c91b873db5ad96584ce9e333c2999a
Roy Ivy III <rivy.dev@gmail.com>
2022-04-10 11:15:38 -0500

    wip ~ WSL support

--- examples/t-clip.rs
+++ examples/t-clip.rs
@@ -1,3 +1,8 @@
+// spell-checker:ignore (jargon) distro (libs) exitcode OSFILE
+
+#[cfg(target_os = "linux")]
+use exitcode;
+
 #[cfg(target_os = "linux")]
 use std::io::Write;
 
@@ -22,9 +27,28 @@ fn main() {
     #[cfg(target_os = "linux")]
     {
         let is_wsl = std::env::var("IS_WSL").is_ok() || std::env::var("WSL_DISTRO_NAME").is_ok();
+        let is_wsl2 = is_wsl && std::env::var("WSL_INTEROP").is_ok();
+        let is_wsl1 = !is_wsl2;
 
-        // ref: <https://stackoverflow.com/questions/5707990/requested-clipboard-operation-did-not-succeed>
-        if !is_wsl {
+        if is_wsl1 {
+            // note: prior opening of the clipboard with WaylandClipboardContext or X11ClipboardContext may leave an open clipboard leak leading to "Requested Clipboard operation did not succeed." errors
+            let mut child = std::process::Command::new("powershell.exe")
+            .args(["-nonInteractive", "-noProfile", "-executionPolicy", "unrestricted", "-command", "$i='';while([Console]::IsInputRedirected -and (($c=[Console]::Read()) -ne -1)){$i+=[Convert]::ToChar($c)}; add-type -AssemblyName System.Windows.Forms; [System.Windows.Forms.Clipboard]::SetText($i)"])
+            .stdin(std::process::Stdio::piped())
+            .stdout(std::process::Stdio::piped())
+            .spawn()
+            .unwrap_or_else(|err| {eprintln!("ERR!: Failed to spawn `powershell.exe` child process ({})", err); std::process::exit(exitcode::OSFILE)});
+            let mut child_stdin = child.stdin.take().expect("Failed to write to STDIN");
+            std::thread::spawn(move || {
+                child_stdin
+                    .write_all(the_string.to_owned().as_bytes())
+                    .expect("Failed to write to STDIN")
+            });
+            let output = child.wait_with_output().expect("Failed to read STDOUT");
+            println!("output = {:?}", output);
+        } else {
+            // * not WSL1
+            // ref: <https://stackoverflow.com/questions/5707990/requested-clipboard-operation-did-not-succeed>
             {
                 let ctx_wayland = WaylandClipboardContext::new();
                 match ctx_wayland {
@@ -52,23 +76,6 @@ fn main() {
                     }
                 }
             }
-        } else {
-            // note: opening the clipboard with WaylandClipboardContext or X11ClipboardContext (as above) may leave an open clipboard leak leading to "Requested Clipboard operation did not succeed." errors
-            let mut child = std::process::Command::new("powershell.exe")
-            // .args(["-nonInteractive", "-noProfile", "-executionPolicy", "unrestricted", "-command", "$i='';while([Console]::IsInputRedirected -and (($c=[Console]::Read()) -ne -1)){$i+=[Convert]::ToChar($c)}; set-clipboard $i"])
-            .args(["-nonInteractive", "-noProfile", "-executionPolicy", "unrestricted", "-command", "$i='';while([Console]::IsInputRedirected -and (($c=[Console]::Read()) -ne -1)){$i+=[Convert]::ToChar($c)}; add-type -AssemblyName System.Windows.Forms; [System.Windows.Forms.Clipboard]::SetText($i)"])
-            .stdin(std::process::Stdio::piped())
-            .stdout(std::process::Stdio::piped())
-            .spawn()
-            .expect("Failed to spawn child process");
-            let mut child_stdin = child.stdin.take().expect("Failed to write to STDIN");
-            std::thread::spawn(move || {
-                child_stdin
-                    .write_all(the_string.to_owned().as_bytes())
-                    .expect("Failed to write to STDIN")
-            });
-            let output = child.wait_with_output().expect("Failed to read STDOUT");
-            println!("output = {:?}", output);
         }
     }
 }
--- examples/t-paste.rs
+++ examples/t-paste.rs
@@ -1,26 +1,22 @@
+// spell-checker:ignore (jargon) distro (libs) exitcode OSFILE
+
+use exitcode;
+use regex::Regex;
+
 use cli_clipboard::{ClipboardContext, ClipboardProvider};
 
-// fn trim_newline(s: String) -> &str {
-//     let mut len = 0;
-//     if s.ends_with('\n') {
-//         len += 1;
-//         if s.ends_with('\r') {
-//             len += 1;
-//         }
-//     } else if s.ends_with('\r') {
-//         len += 1;
-//     }
-//     &s[0..s.len() - len]
-// }
-
 fn main() {
+    // ref: [Detect WSL1 vs WSL2](https://github.com/microsoft/WSL/issues/4555) @@ <https://archive.is/uDc9K>
     let is_wsl = std::env::var("IS_WSL").is_ok() || std::env::var("WSL_DISTRO_NAME").is_ok();
+    let is_wsl2 = is_wsl && std::env::var("WSL_INTEROP").is_ok();
+    let is_wsl1 = !is_wsl2;
 
-    if cfg!(windows) || !is_wsl {
+    if cfg!(windows) || !is_wsl1 {
         let mut ctx = ClipboardContext::new().unwrap();
         let s = ctx.get_contents().unwrap();
         print!("{}", s);
     } else {
+        // is_wsl
         let output = std::process::Command::new("powershell.exe")
             .args([
                 "-nonInteractive",
@@ -31,8 +27,11 @@ fn main() {
                 "add-type -AssemblyName System.Windows.Forms; [System.Windows.Forms.Clipboard]::GetText()",
             ])
             .output()
-            .expect("Failed to spawn child process");
-        let s = String::from_utf8_lossy(&output.stdout);
-        print!("{}", s.trim_end_matches(|c| c == '\n' || c == '\r'));
+            .unwrap_or_else(|err| {eprintln!("ERR!: `powershell.exe` child process failed ({})", err); std::process::exit(exitcode::OSFILE)});
+        let output = String::from_utf8_lossy(&output.stdout);
+        // trim trailing newline (removes the unavoidable newline added by `powershell.exe`)
+        let rx = Regex::new(r"\r?\n?$").unwrap();
+        let s = rx.replace(&output, "");
+        print!("{}", s);
     }
 }
